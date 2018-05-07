d25c6334e986c251cda1fd332c4a412eb4024cf0
Rahul Kejriwal <kejriwalrahul@outlook.com>
2018-05-08 01:59:36 +0530

    bug fix

--- Code/measure.py
+++ Code/measure.py
@@ -5,6 +5,7 @@
 """
 import re
 import sys
+import collections
 
 out_file = sys.argv[1]
 
@@ -13,11 +14,16 @@ with open(out_file) as fp:
 
 seg_re = re.compile('\[\[(.*?)\]\]')
 matches = re.findall(seg_re, segmentation)
+matches = list(matches)
 
 count = 0
 for i in range(len(matches)-1):
-	if matches[i].endswith('_') or matches[i+1].startswith('_'):
+	if matches[i].endswith('_') or (matches[i+1].startswith('_') and matches[i+1]!='_'):
 		count += 1
+
+c1 = collections.Counter(matches)
+
+print count, len(matches), (segmentation.count('_'))
 
 p, r = float(count) / (len(matches)-1), float(count) / (segmentation.count('_'))
 print "Precision: ", p
